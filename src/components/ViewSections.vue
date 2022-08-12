86bc2ced6fa523805a82bcf7c01ab5ea61f63734
jaredfigdor <jaredfigdor@gmail.com>
2022-08-12 17:35:24 -0400

    new

--- src/components/ViewSections.vue
+++ src/components/ViewSections.vue
@@ -67,26 +67,53 @@
       slider-color="yellow"
     >
       <v-tab
-        v-for="n in 2"
-        :key="n"
-        ripple
-      >
-        Item {{ n }}
+       
+       >
+        Policy
+
+      </v-tab>
+         <v-tab
+       
+       >
+        modification
 
       </v-tab>
       <v-tab-item
-        v-for="n in 2"
-        :key="n"
+      
       >
         <v-card flat>
-          <v-card-text>{{ policy }}</v-card-text>
+          
+          <v-card-text>
+
+{{policy}}
+
+          </v-card-text>
+          
         </v-card>
+
+        
       </v-tab-item>
+
+            <v-tab-item
+      
+      >
+        <v-card flat>
+          
+          <v-card-text>
+
+{{modification}}
+
+          </v-card-text>
+          
+        </v-card>
+
+        
+      </v-tab-item>
+
+      
     </v-tabs>
 
-    <div class="text-xs-center mt-3">
-      <v-btn @click="next">next tab</v-btn>
-    </div>
+
   </div>
 
 </div>
@@ -101,7 +128,7 @@
             </v-btn>
           </div>
           <div class="col">
-            <v-btn v-bind:to="{name:'edit-sections', params: {section_id: section_id}}" class="waves-effect waves-light btn red" 
+            <v-btn  
               
               color="red"
               dark
@@ -127,14 +154,22 @@
 </template>
 
 <script>
-
+import { Editor, EditorContent } from '@tiptap/vue-2'
+import StarterKit from '@tiptap/starter-kit'
 import db from './firebaseInit'
 import router from '../router'
 export default {
-
+components: {EditorContent,},
 name: 'view-sections',
+ props: {
+    value: {
+      type: String,
+      default: '',
+    },
+  },
 data () {
     return {
+      editor: null,
         active: null,
      section_id: null,
      name: null,
@@ -165,13 +200,42 @@ beforeRouteEnter(to, from, next) {
     })
     
 },
-watch: {
-    '$route': 'fetchData'
-},
+ watch: {
+    value(value) {
+      // HTML
+      const isSame = this.editor.getHTML() === value
+
+      // JSON
+      // const isSame = JSON.stringify(this.editor.getJSON()) === JSON.stringify(value)
+
+      if (isSame) {
+        return
+      }
+
+      this.editor.commands.setContent(value, false)
+    },
+  },
+mounted() {
+    this.editor = new Editor({
+      extensions: [
+        StarterKit,
+      ],
+      content: this.policy,
+    })
+  },
+
+  beforeUnmount() {
+    this.editor.destroy()
+  },
+
+
+
+
 methods: {
       next () {
         const active = parseInt(this.active)
         this.active = (active < 2 ? active + 1 : 0)
+        
       }
     }
 
@@ -236,6 +300,60 @@ i {
     color: aliceblue;
 }
 
+.ProseMirror {
+  > * + * {
+    margin-top: 0.75em;
+  }
 
+  ul,
+  ol {
+    padding: 0 1rem;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    line-height: 1.1;
+  }
+
+  code {
+    background-color: rgba(#616161, 0.1);
+    color: #616161;
+  }
+
+  pre {
+    background: #0D0D0D;
+    color: #FFF;
+    font-family: 'JetBrainsMono', monospace;
+    padding: 0.75rem 1rem;
+    border-radius: 0.5rem;
+
+    code {
+      color: inherit;
+      padding: 0;
+      background: none;
+      font-size: 0.8rem;
+    }
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  blockquote {
+    padding-left: 1rem;
+    border-left: 2px solid rgba(#0D0D0D, 0.1);
+  }
+
+  hr {
+    border: none;
+    border-top: 2px solid rgba(#0D0D0D, 0.1);
+    margin: 2rem 0;
+  }
+}
 
 </style>
